87abb7a3520e1d24770085e489223746b0fc68f1
evaniewares <chisopsyelera@gmail.com>
2023-07-26 15:49:20 +0000

    Printf

--- _printf.c
+++ _printf.c
@@ -15,11 +15,8 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (!format || (format[0] == '%' && !format[1]))
-	{
-		return (-1);
-	}
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
+
+	if (format == NULL)
 	{
 		return (-1);
 	}
